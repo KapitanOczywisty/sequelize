77baf861ad5781d8fe31beaef32639f463009da7
KapitanOczywisty <44417092+KapitanOczywisty@users.noreply.github.com>
2021-02-06 20:22:38 +0100

    fix(types): models with attributes cannot be used in some functions

--- types/lib/model-manager.d.ts
+++ types/lib/model-manager.d.ts
@@ -1,15 +1,15 @@
-import { Model } from './model';
+import { Model, ModelType } from './model';
 import { Sequelize } from './sequelize';
 
 export class ModelManager {
   public sequelize: Sequelize;
-  public models: typeof Model[];
-  public all: typeof Model[];
+  public models: ModelType[];
+  public all: ModelType[];
 
   constructor(sequelize: Sequelize);
-  public addModel<T extends typeof Model>(model: T): T;
-  public removeModel(model: typeof Model): void;
-  public getModel(against: unknown, options?: { attribute?: string }): typeof Model;
+  public addModel<T extends ModelType>(model: T): T;
+  public removeModel(model: ModelType): void;
+  public getModel(against: unknown, options?: { attribute?: string }): ModelType;
 }
 
 export default ModelManager;
